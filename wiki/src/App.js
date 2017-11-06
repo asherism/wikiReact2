7cf1572262336f4a8bf9b65ebb9680b32ff641ef
Yashar Asheri <yasharasheri@icloud.com>
2017-11-06 15:43:21 -0400

    updated with colors

--- wiki/src/App.js
+++ wiki/src/App.js
@@ -1,33 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './App.css';
-import getConcatArray from './wiki.js'
+import getConcatArray from "./wiki.js";
 
-
-let words = ""
+let words = "";
 class App extends Component {
-
-
   componentWillMount() {
     getConcatArray()
       .then(concatArray => {
-        console.log(concatArray)
+        console.log(concatArray);
         this.setState({
           words: concatArray,
           process: false
-        })
+        });
       })
       .catch(err => {
         console.log(err);
-      })
+      });
   }
 
   componentDidUpdate() {
-    console.log('updated');
+    console.log("updated");
     if (this.state.process === true) {
       this.setState({
         updated: true
-      })
+      });
     }
   }
 
@@ -37,159 +33,182 @@ class App extends Component {
       words: [],
       process: true,
       updated: false
-    }
+    };
   }
 
   render() {
-
-
     let loadingAnim;
+    let colors = [
+      "AliceBlue",
+      "AntiqueWhite",
+      "Aqua",
+      "Aquamarine",
+      "Azure",
+      "Beige",
+      "Bisque",
+      "Black",
+      "BlanchedAlmond",
+      "Blue",
+      "BlueViolet",
+      "Brown",
+      "BurlyWood",
+      "CadetBlue",
+      "Chartreuse",
+      "Chocolate",
+      "Coral",
+      "CornflowerBlue",
+      "Cornsilk",
+      "Cyan",
+      "DarkBlue",
+      "DarkCyan",
+      "DarkGoldenRod",
+      "DarkGray",
+      "DarkGrey",
+      "DarkGreen",
+      "DarkKhaki",
+      "DarkMagenta",
+      "Darkorange",
+      "DarkRed",
+      "DarkSalmon",
+      "DarkSeaGreen",
+      "DarkSlateBlue",
+      "DarkSlateGray",
+      "DarkSlateGrey",
+      "DarkTurquoise",
+      "DarkViolet",
+      "DeepPink",
+      "DeepSkyBlue",
+      "DodgerBlue",
+      "FloralWhite",
+      "ForestGreen",
+      "Fuchsia",
+      "Gainsboro",
+      "GhostWhite",
+      "Gold",
+      "GoldenRod",
+      "GreenYellow",
+      "HoneyDew",
+      "HotPink",
+      "IndianRed",
+      "Indigo",
+      "Ivory",
+      "Khaki",
+      "Lavender",
+      "LavenderBlush",
+      "LawnGreen",
+      "LemonChiffon",
+      "LightBlue",
+      "LightCoral",
+      "LightCyan",
+      "LightGoldenRodYellow",
+      "LightGray",
+      "LightGrey",
+      "LightGreen",
+      "LightPink",
+      "LightSalmon",
+      "LightSeaGreen",
+      "LightSkyBlue",
+      "LightSlateGray",
+      "LightSlateGrey",
+      "LightSteelBlue",
+      "LightYellow",
+      "Lime",
+      "LimeGreen",
+      "Linen",
+      "Magenta",
+      "Maroon",
+      "MediumAquaMarine",
+      "MediumBlue",
+      "MediumOrchid",
+      "MediumSeaGreen",
+      "MediumSpringGreen",
+      "MediumTurquoise",
+      "MidnightBlue",
+      "MintCream",
+      "MistyRose",
+      "Moccasin",
+      "NavajoWhite",
+      "Navy",
+      "OldLace",
+      "Olive",
+      "OliveDrab",
+      "Orange",
+      "OrangeRed",
+      "Orchid",
+      "PaleGoldenRod",
+      "PaleGreen",
+      "PaleTurquoise",
+      "PaleVioletRed",
+      "PapayaWhip",
+      "PeachPuff",
+      "Peru",
+      "Pink",
+      "Plum",
+      "PowderBlue",
+      "Purple",
+      "Red",
+      "RosyBrown",
+      "SaddleBrown",
+      "Salmon",
+      "SandyBrown",
+      "SeaGreen",
+      "SeaShell",
+      "Sienna",
+      "Silver",
+      "SkyBlue",
+      "SlateGray",
+      "SlateGrey",
+      "Snow",
+      "SpringGreen",
+      "SteelBlue",
+      "Tan",
+      "Thistle",
+      "Tomato",
+      "Turquoise",
+      "Violet",
+      "Wheat",
+      "White",
+      "WhiteSmoke",
+      "Yellow",
+      "YellowGreen"
+    ];
 
     if (this.state.process === true) {
-      loadingAnim = (
-        <div className="wrapper">
-          <div className="line line1">
-            <span className="circle circle-top"></span>
-            <div className="dotted">
-              <span className="dot dot-top"></span>
-              <span className="dot dot-middle-top"></span>
-              <span className="dot dot-middle-bottom"></span>
-              <span className="dot dot-bottom"></span>
-            </div>
-            <span className="circle circle-bottom"></span>
-          </div>
-          <div className="line line2">
-            <span className="circle circle-top"></span>
-            <div className="dotted">
-              <span className="dot dot-top"></span>
-              <span className="dot dot-middle-top"></span>
-              <span className="dot dot-middle-bottom"></span>
-              <span className="dot dot-bottom"></span>
-            </div>
-            <span className="circle circle-bottom"></span>
-          </div>
-          <div className="line line3">
-            <span className="circle circle-top"></span>
-            <div className="dotted">
-              <span className="dot dot-top"></span>
-              <span className="dot dot-middle-top"></span>
-              <span className="dot dot-middle-bottom"></span>
-              <span className="dot dot-bottom"></span>
-            </div>
-            <span className="circle circle-bottom"></span>
-          </div>
-          <div className="line line4">
-            <span className="circle circle-top"></span>
-            <div className="dotted">
-              <span className="dot dot-top"></span>
-              <span className="dot dot-middle-top"></span>
-              <span className="dot dot-middle-bottom"></span>
-              <span className="dot dot-bottom"></span>
-            </div>
-            <span className="circle circle-bottom"></span>
-          </div>
-          <div className="line line5">
-            <span className="circle circle-top"></span>
-            <div className="dotted">
-              <span className="dot dot-top"></span>
-              <span className="dot dot-middle-top"></span>
-              <span className="dot dot-middle-bottom"></span>
-              <span className="dot dot-bottom"></span>
-            </div>
-            <span className="circle circle-bottom"></span>
-          </div>
-          <div className="line line6">
-            <span className="circle circle-top"></span>
-            <div className="dotted">
-              <span className="dot dot-top"></span>
-              <span className="dot dot-middle-top"></span>
-              <span className="dot dot-middle-bottom"></span>
-              <span className="dot dot-bottom"></span>
-            </div>
-            <span className="circle circle-bottom"></span>
-          </div>
-          <div className="line line7">
-            <span className="circle circle-top"></span>
-            <div className="dotted">
-              <span className="dot dot-top"></span>
-              <span className="dot dot-middle-top"></span>
-              <span className="dot dot-middle-bottom"></span>
-              <span className="dot dot-bottom"></span>
-            </div>
-            <span className="circle circle-bottom"></span>
-          </div>
-          <div className="line line8">
-            <span className="circle circle-top"></span>
-            <div className="dotted">
-              <span className="dot dot-top"></span>
-              <span className="dot dot-middle-top"></span>
-              <span className="dot dot-middle-bottom"></span>
-              <span className="dot dot-bottom"></span>
-            </div>
-            <span className="circle circle-bottom"></span>
-          </div>
-          <div className="line line9">
-            <span className="circle circle-top"></span>
-            <div className="dotted">
-              <span className="dot dot-top"></span>
-              <span className="dot dot-middle-top"></span>
-              <span className="dot dot-middle-bottom"></span>
-              <span className="dot dot-bottom"></span>
-            </div>
-            <span className="circle circle-bottom"></span>
-          </div>
-          <div className="line line10">
-            <span className="circle circle-top"></span>
-            <div className="dotted">
-              <span className="dot dot-top"></span>
-              <span className="dot dot-middle-top"></span>
-              <span className="dot dot-middle-bottom"></span>
-              <span className="dot dot-bottom"></span>
-            </div>
-            <span className="circle circle-bottom"></span>
-          </div>
-          <div className="line line11">
-            <span className="circle circle-top"></span>
-            <div className="dotted">
-              <span className="dot dot-top"></span>
-              <span className="dot dot-middle-top"></span>
-              <span className="dot dot-middle-bottom"></span>
-              <span className="dot dot-bottom"></span>
-            </div>
-            <span className="circle circle-bottom"></span>
-          </div>
-        </div>)
+      loadingAnim = <p>Loading...</p>;
     }
 
     if (this.state.process === false) {
-      loadingAnim = (this.state.words.map((element, int) => {
-
+      loadingAnim = this.state.words.map((element, index) => {
         let animation = {
-
-          animationDuration: `${int / 2}s`,
+          animationDuration: `${index / 2}s`,
           animationFillMode: "both",
-          animationDelay: int,
-          color: `rgb(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)})`,
+          animationDelay: index,
+          color: colors[Math.floor(Math.random() * colors.length)],
           display: "block",
           padding: "10px",
           fontSize: "40px"
-        }
+        };
 
-        return <a target="_blank" href={"https://en.wikipedia.org/wiki/" + element.replace(/ /g, "_")} style={animation} className="slideInDown">{element}</a>
-      }))
+        return (
+          <a
+            target="_blank"
+            href={"https://en.wikipedia.org/wiki/" + element.replace(/ /g, "_")}
+            style={animation}
+            className="slideInDown"
+            key={index}
+          >
+            {index + 1}. {element} 🔗
+          </a>
+        );
+      });
     }
 
     return (
       <div className="App">
-        <div className="arrayDisplay">
-
-          {loadingAnim}
-
-        </div>
+        <div className="arrayDisplay">{loadingAnim}</div>
       </div>
     );
   }
 }
-
 
 export default App;
--- wiki/src/wiki.js
+++ wiki/src/wiki.js
@@ -1,10 +1,4 @@
-
-import wiki from 'wikijs';
-
-
-
-
-
+import wiki from "wikijs";
 
 let wikiArray = [];
 let concatArray = [];
@@ -12,101 +6,100 @@ let newWordQuery = "";
 let newWordArray = [];
 let secondSearchArray = [];
 
-
 function randomize() {
-    return new Promise((resolve, reject) => {
-        wiki().random()
-            .then(function (page) {
-                //if random results ends in "disambiguation" run random again
-                concatArray = wikiArray.concat(page)
-                console.log(concatArray + " A")
-            })
-            .then(() => {
-                let lastSentence = concatArray[concatArray.length - 1];
-                // console.log(lastSentence + " B");
+  return new Promise((resolve, reject) => {
+    wiki()
+      .random()
+      .then(function(page) {
+        //if random results ends in "disambiguation" run random again
+        concatArray = wikiArray.concat(page);
+        console.log("Starting thread...", concatArray);
+      })
+      .then(() => {
+        let lastSentence = concatArray[concatArray.length - 1];
+        // console.log(lastSentence + " B");
 
-                let newWordQuery = lastSentence.split(' ').pop().replace(/[{()}]/g, "");
-                //console.log(newWordQuery + " C");
-                //concatArray.push(newWordQuery);
-                resolve(newWordQuery)
-
-            })
-            .catch((err) => {
-                reject(err)
-            })
-
-    });
-
-
+        let newWordQuery = lastSentence
+          .split(" ")
+          .pop()
+          .replace(/[{()}]/g, "");
+        //console.log(newWordQuery + " C");
+        //concatArray.push(newWordQuery);
+        resolve(newWordQuery);
+      })
+      .catch(err => {
+        reject(err);
+      });
+  });
 }
 
 function getConcatArray() {
-    return new Promise((resolve, reject) => {
-        const randomPromise = randomize()
+  return new Promise((resolve, reject) => {
+    const randomPromise = randomize();
 
-        randomPromise.then((newWordQuery) => {
-            randomSearch(newWordQuery, (err, concatArray) => {
-                console.log(concatArray);
-                return resolve(concatArray);
-            })
-        })
-    })
+    randomPromise.then(newWordQuery => {
+      randomSearch(newWordQuery, (err, concatArray) => {
+        console.log(concatArray);
+        return resolve(concatArray);
+      });
+    });
+  });
 }
 // console.log(randomPromise)
-String.prototype.capitalize = function () {
-    return this.charAt(0).toUpperCase() + this.slice(1);
-}
+String.prototype.capitalize = function() {
+  return this.charAt(0).toUpperCase() + this.slice(1);
+};
 
 function randomSearch(randomPromise, cb) {
-    //return new Promise((resolve, reject) => {
-        wiki().search(randomPromise, 100)
-            .then((data) => {
-                // console.log(data.results);
-                for (let i = 0; i < data.results.length; i++) {
-                    if (data.results[i].split(" ")[0] === randomPromise.capitalize()
-                        && data.results[i].split(" ").length > 1
-                        && data.results[i].split(" ")[data.results[i].split(' ').length - 1] !== "(disambiguation)") {
-                        // console.log(data.results[i])
-                        newWordArray.push(data.results[i])
-                        // pushing results into newWordArray to get random search query
-                    }
+  //return new Promise((resolve, reject) => {
+  wiki()
+    .search(randomPromise, 100)
+    .then(data => {
+      // console.log(data.results);
+      for (let i = 0; i < data.results.length; i++) {
+        if (
+          data.results[i].split(" ")[0] === randomPromise.capitalize() &&
+          data.results[i].split(" ").length > 1 &&
+          data.results[i].split(" ")[data.results[i].split(" ").length - 1] !==
+            "(disambiguation)"
+        ) {
+          // console.log(data.results[i])
+          newWordArray.push(data.results[i]);
+          // pushing results into newWordArray to get random search query
+        }
+      }
+      if (newWordArray.length > 1) {
+        // console.log(newWordArray + " D")
+        let secondSearchQeury =
+          newWordArray[Math.floor(Math.random() * newWordArray.length)];
+        concatArray.push(secondSearchQeury);
+        let newSecondSearchQuery = secondSearchQeury
+          .split(" ")
+          .pop()
+          .replace(/[{()}]/g, "");
+        //let newWordQuery = newSecondSearchQuery;
+        newWordArray = [];
+        if (concatArray.length >= 10) {
+          console.log("here");
+          return cb(null, concatArray);
+        }
+        randomSearch(newSecondSearchQuery.capitalize(), cb);
+        // console.log(newSecondSearchQuery + " E");
+        console.log(concatArray);
+      } else {
+        wikiArray = [];
+        concatArray = [];
+        newWordQuery = "";
+        newWordArray = [];
+        secondSearchArray = [];
+        //randomize();
+        randomize().then(newWordQuery => {
+          let searchPromise = randomSearch(newWordQuery, cb);
+        });
+      }
+    });
 
-                }
-                if (newWordArray.length > 1) {
-                    console.log(newWordArray + " D")
-                    let secondSearchQeury = newWordArray[Math.floor(Math.random() * newWordArray.length)]
-                    concatArray.push(secondSearchQeury);
-                    let newSecondSearchQuery = secondSearchQeury.split(" ").pop().replace(/[{()}]/g, "");
-                    //let newWordQuery = newSecondSearchQuery;
-                    newWordArray = [];
-                    if (concatArray.length >= 25) {
-                        console.log('here');
-                       return cb(null, concatArray);
-                    }
-                    randomSearch(newSecondSearchQuery.capitalize(), cb);
-                    console.log(newSecondSearchQuery + " E");
-                    console.log(concatArray);
-
-
-
-                } else {
-                    wikiArray = [];
-                    concatArray = [];
-                    newWordQuery = "";
-                    newWordArray = [];
-                    secondSearchArray = [];
-                    //randomize();
-                    randomize().then((newWordQuery) => {
-                        let searchPromise = randomSearch(newWordQuery, cb)
-                    })
-                }
-            })
-
-   // })
-
+  // })
 }
 
-export default getConcatArray
-
-
-
+export default getConcatArray;
